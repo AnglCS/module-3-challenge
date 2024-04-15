597ab0dd69d064079d8e9a27bd7434ca005c8d2d
An <angelcdsv@gmail.com>
2024-04-15 13:21:34 -0600

    deleted starter code first part to script.js

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-// Get a reference to the #add-employees-btn element
-const addEmployeesBtn = document.querySelector('#add-employees-btn');
-
-// Collect employee data
-const collectEmployees = function() {
-  // TODO: Get user input to create and return an array of employee objects
-}
-
-// Display the average salary
-const displayAverageSalary = function(employeesArray) {
-  // TODO: Calculate and display the average salary
-}
-
-// Select a random employee
-const getRandomEmployee = function(employeesArray) {
-  // TODO: Select and display a random employee
-}
 
 /*
   ====================
